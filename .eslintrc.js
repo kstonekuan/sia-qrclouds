c74710b53c434387ded09d70402d758755d7939f
h1110050 <h1110050@github.com>
2019-08-11 16:09:34 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,25 @@
 module.exports = {
-    'extends': 'airbnb',
-    'parser': 'babel-eslint',
-    'plugins': [
-      'react',
-    ],
-    'env': {
-      'jest': true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'rules': {
-      'brace-style': ["error", "1tbs"],
-      'class-methods-use-this': 1,
-      'comma-dangle': 0,
-      'consistent-return': 0,
-      'default-case': 0,
-      'global-require' : 0,
-      'import/no-unresolved': 1,
-      'linebreak-style' : 0,
-      'max-len' : 0,
-      'no-console': 0,
-      'no-unused-vars' : 1,
-      'no-use-before-define': 0,
-      'object-curly-newline' : 0,
-      'object-shorthand' : 0,
-      'react/destructuring-assignment': 1,
-      'react/jsx-filename-extension': 0,
-      'react/prefer-stateless-function': 0,
-      'react/prop-types': 0,
-      'react/sort-comp': 0,
-    },
-    'globals': {
-      "fetch": false
-    }
-  }
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
